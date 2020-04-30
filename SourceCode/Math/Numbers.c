3eb36a047e4a66e61b66e27184cf429e75df03c0
ssaahhaajj <ssaahhaajj@gmail.com>
2020-04-30 16:31:45 +0530

    Refactored addition function

--- SourceCode/Math/Numbers.c
+++ SourceCode/Math/Numbers.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 #include<math.h>
-
+// in bugFix branch
 int sum(int x,int y)
 {
-  return x+y;
+  int result =0;
+  result=x+y;
+  return result;
 }
 //added to testing branch
 int mulitply(int x,int y)
